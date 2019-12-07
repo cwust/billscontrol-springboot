a717b3302c78717f69359ab555d091c98c383b47
Charles Wust <charles.wust@gmail.com>
2019-12-07 15:42:27 -0300

    Initial commit

---
+++ src/main/java/br/cwust/billscontrol/BillscontrolApplication.java
@@ -0,0 +1,13 @@
+package br.cwust.billscontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillscontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillscontrolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/cwust/billscontrol/BillscontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package br.cwust.billscontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillscontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
